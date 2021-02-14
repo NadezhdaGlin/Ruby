39400e2530436e691f75cb25beee7b0bc7c67fa7
Nadezhda <glinovanad@gmail.com>
2021-02-14 18:29:18 +0300

    опрос (проблема с первым вопросом)

---
+++ Lab1/1_3.rb
@@ -0,0 +1,11 @@
+puts "Какой ваш любимый язык?"
+first = ARGV[0] 
+if first == "Ruby"
+	puts "Да вы подлиза ( ͡° ͜ʖ ͡°)"
+elsif
+	first == "ruby" 
+	puts "Да вы подлиза ( ͡° ͜ʖ ͡°)"
+else
+	puts "Скоро будет Ruby (・`ω´・)"
+end
+#puts "#{first}"
