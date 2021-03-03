4e3b397d0b2cdc16a94c823eb6ea94cb834c9419
Nadezhda <glinovanad@gmail.com>
2021-03-03 16:12:56 +0300

    2  лаб 3 задание (push)

--- Lab2/2_3.rb
+++ Lab2/2_3.rb
@@ -7,3 +7,13 @@ mas = Array.new
 mas
 end
 puts first().inspect
+
+def first()
+n = ARGV[0].to_i
+mas = Array.new
+	for i in 0..n-1
+		mas.push($stdin.gets.chomp.to_i)
+	end
+mas
+end
+puts first().inspect
