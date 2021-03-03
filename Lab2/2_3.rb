8ed85ced525ee1ef23eeff37fac552b0509d25d5
Nadezhda <glinovanad@gmail.com>
2021-03-03 16:47:54 +0300

    2 лаб 3 задание (insert)

--- Lab2/2_3.rb
+++ Lab2/2_3.rb
@@ -18,11 +18,21 @@
 # end
 # puts second().inspect
 
+# def first()
+# n = ARGV[0].to_i
+# mas = Array.new
+# 	for i in 0..n-1
+# 		mas.unshift($stdin.gets.chomp.to_i)
+# 	end
+# mas
+# end
+# puts first().inspect
+
 def first()
 n = ARGV[0].to_i
 mas = Array.new
 	for i in 0..n-1
-		mas.unshift($stdin.gets.chomp.to_i)
+		mas.insert(mas.length, $stdin.gets.chomp.to_i)
 	end
 mas
 end
