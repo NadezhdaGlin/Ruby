38f692d2a0e58c3f96e4a4c96e3ad39dba1c2d2d
Nadezhda <glinovanad@gmail.com>
2021-03-03 16:43:51 +0300

    2 лаб 3 задание (unshift)

--- Lab2/2_3.rb
+++ Lab2/2_3.rb
@@ -1,18 +1,28 @@
-def first()
-n = ARGV[0].to_i
-mas = Array.new
-	for i in 0..n-1
-		mas << $stdin.gets.chomp.to_i
-	end
-mas
-end
-puts first().inspect
+# def first()
+# n = ARGV[0].to_i
+# mas = Array.new
+# 	for i in 0..n-1
+# 		mas << $stdin.gets.chomp.to_i
+# 	end
+# mas
+# end
+# puts first().inspect
+
+# def second()
+# n = ARGV[0].to_i
+# mas = Array.new
+# 	for i in 0..n-1
+# 		mas.push($stdin.gets.chomp.to_i)
+# 	end
+# mas
+# end
+# puts second().inspect
 
 def first()
 n = ARGV[0].to_i
 mas = Array.new
 	for i in 0..n-1
-		mas.push($stdin.gets.chomp.to_i)
+		mas.unshift($stdin.gets.chomp.to_i)
 	end
 mas
 end
