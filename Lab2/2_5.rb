5551ede4a3774682831371b8f86527b3702ac3af
Nadezhda <glinovanad@gmail.com>
2021-03-07 14:17:58 +0300

    2 лаб 5 задание (done)

--- Lab2/2_5.rb
+++ Lab2/2_5.rb
@@ -1,25 +1,20 @@
- puts "Выберите цифру, каким образом вы хотите получить значение. \n 
- 1 - чтение из файла \n 2 - ввод вручную"
- number = gets.chomp.to_i
- case number
- 	when "1" then puts vvod()
- 	when "2" then puts vvod2()
- else
- 	puts "Вы ввели не то число"
- end
-
+ # puts "Выберите цифру, каким образом вы хотите получить значение. \n 
+ # 1 - чтение из файла \n 2 - ввод вручную"
+ # number = $stdin.gets.chomp.to_i
 
 def vvod()
-	first = ARGV[0]
+	first = $stdin.gets.chomp
 	mas = Array.new
 	file = File.open("#{first}").each do |line|
 		mas << line.chomp
 	end
+	file.close
 	mas
 end
-puts vvod().inspect
+# puts vvod().inspect
 
 def vvod2()
+	puts "Вводите\n"
 	num = $stdin.gets.chomp
 	mas = Array.new
 	while !num.empty?
@@ -30,8 +25,14 @@ def vvod2()
 	end
 	mas
 end
-puts vvod2().inspect
+# puts vvod2().inspect
 
+case ARGV[0].chomp.to_i
+ 	when 1 then puts vvod()
+ 	when 2 then puts vvod2().inspect
+ else
+ 	puts "Вы ввели не то число"
+ end
  # puts "Выберите цифру, каким образом вы хотите получить значение. \n 
  # 1 - чтение из файла \n 2 - ввод вручную"
  # number = gets.chomp.to_i
