86ea9ec050bd3b6249cf2d1346e2726a8994815b
Nadezhda <glinovanad@gmail.com>
2021-03-13 04:02:19 +0300

    3 лаб 3 задание (2.5/5)

---
+++ Lab3/3_3.rb
@@ -0,0 +1,16 @@
+c_p = File.dirname( __FILE__)
+require "#{c_p}/Employee.rb"
+
+def self.checkPhone?(phone)
+	result = phone =~ /^(8|\+7)([\s\(\)\-]*\d){10}$/
+	return result.nil?
+	phone = phone.strip
+	if Employee.checkPhone?(phone.strip)
+		raise "Номер введен не верно"
+	end
+
+	digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+		numbers = phone.chars.filter { |i| digits.include?(i) }
+		numbers.insert(1, "-")
+		numbers.insert(5, "-")
+end
