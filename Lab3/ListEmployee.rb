b275542c47a8f0944016642d2355c21205e8768d
Nadezhda <glinovanad@gmail.com>
2021-05-14 13:56:00 +0300

    3_14 улучшенное чтение и добавление

--- Lab3/ListEmployee.rb
+++ Lab3/ListEmployee.rb
@@ -4,44 +4,43 @@ require_relative "3.rb"
 	# file.puts(a)
 # end
 class ListEmployee
-	attr_accessor :mas
 
 	def initialize()
-		self.mas = Array.new()
+		@mas = Array.new()
 	end
 
-	def add()
-		puts "Введите ФИО"
-		fio = gets.chomp
-		puts "Введите Дату"
-		date = gets.chomp
-		puts "Введите телефон"
-		phone = gets.chomp
-		puts "Адрес"
-		addr = gets.chomp
-		puts "Введите email"
-		mail = gets.chomp
-		puts "Введите паспорт"
-		pas = gets.chomp
-		puts "Стаж работы"
-		st = gets.chomp.to_i
-		if st > 0
-			puts "Прошлое место работы"	
-			lastwork = gets.chomp
-			puts "Наименование"
-			jobtitle = gets.chomp
-			puts "Зарплата"
-			salary = gets.chomp.to_i
-			self.mas << Employee.new(fio, date, phone, addr, mail, pas, st, lastwork, jobtitle, salary)
-			file_write = File.open('Employee.txt', 'a') do |file| 
-				 file.puts(self.mas)
-			end
-		else
-			self.mas << Employee.new(fio, date, phone, addr, mail, pas, st)
-			file_write = File.open('Employee.txt', 'a') do |file| 
-				 file.puts(self.mas)
-			end
-		end
+	def add(employee)
+		# puts "Введите ФИО"
+		# fio = gets.chomp
+		# puts "Введите Дату"
+		# date = gets.chomp
+		# puts "Введите телефон"
+		# phone = gets.chomp
+		# puts "Адрес"
+		# addr = gets.chomp
+		# puts "Введите email"
+		# mail = gets.chomp
+		# puts "Введите паспорт"
+		# pas = gets.chomp
+		# puts "Стаж работы"
+		# st = gets.chomp.to_i
+		# if st > 0
+		# 	puts "Прошлое место работы"	
+		# 	lastwork = gets.chomp
+		# 	puts "Наименование"
+		# 	jobtitle = gets.chomp
+		# 	puts "Зарплата"
+		# 	salary = gets.chomp.to_i
+			@mas << employee #Employee.new(fio, date, phone, addr, mail, pas, st, lastwork, jobtitle, salary)
+	# 		file_write = File.open('Employee.txt', 'a') do |file| 
+	# 			 file.puts(self.mas)
+	# 		end
+	# 	else
+	# 		self.mas << Employee.new(fio, date, phone, addr, mail, pas, st)
+	# 		file_write = File.open('Employee.txt', 'a') do |file| 
+	# 			 file.puts(self.mas)
+	# 		end
+	# 	end
 	end
 	def read_from_file() #ПЕРЕДЕЛАТЬ под чтение из файла
 		read_data = File.open('Employee.txt', 'r') do |file|
@@ -57,19 +56,29 @@ class ListEmployee
 					lastwork = file.readline().chomp.strip
 					jobtitle = file.readline().chomp.strip
 					salary = file.readline().to_i
-					self.mas << Employee.new(fio, date, phone, addr, mail, pas, st, lastwork, jobtitle, salary)
+					add(Employee.new(fio, date, phone, addr, mail, pas, st, lastwork, jobtitle, salary))
 				else
-					self.mas << Employee.new(fio, date, phone, addr, mail, pas, st)
+					add(Employee.new(fio, date, phone, addr, mail, pas, st))
 				end
 			end
 		end
 	end
-	def show()
-
+	
+	def show() 
+		str = ""
+		@mas.each {|el| str = str + el.show + "\n"}
+		str
 	end
+
+	def search_fio()
+		
+	end
+
+
+
 end
 
 object = ListEmployee.new()
 # puts object.add()
 object.read_from_file()
-puts object.mas
+puts object.show
