b74fb49bab1619cd1ee1307c148ee6513bbb5fb6
Nadezhda <glinovanad@gmail.com>
2021-07-03 00:22:45 +0300

    теперь точно это хэш

--- Tests/parameters.rb
+++ Tests/parameters.rb
@@ -1,15 +1,44 @@
 class Parameters
-	attr_accessor :Weight,
-	:Length,
-	:Width,
-	:Height
 
-	def initialize(options = {})
-		self.Weight= options[:weight]
-		self.Length= options[:length]
-		self.Width= options[:width]
-		self.Height= options[:height]
+	def self.data
+		puts "Введите параметры перевозимого груза - вес(кг), длина(см), ширина(см), высота(см)"
+		puts "Вес"
+		weight = gets.chomp.to_i
+		puts "длина"
+		length = gets.chomp.to_i
+		puts "ширина"
+		width = gets.chomp.to_i
+		puts "высота"
+		height = gets.chomp.to_i
+		{weight:weight, length:length, width:width, height:height}
 	end
+
+end
+puts Parameters.data
+# 	attr_accessor :Weight,
+# 	:Length,
+# 	:Width,
+# 	:Height
+
+# 	def initialize(options = {})
+# 		self.Weight= options[:weight]
+# 		self.Length= options[:length]
+# 		self.Width= options[:width]
+# 		self.Height= options[:height]
+# 	end
+
+
+# 	def show
+# 		"Вес #{self.Weight} кг
+# Длина #{self.Length} см
+# Ширина #{self.Width} см
+# Высота #{self.Height} см"
+# 	end
+
+# end
+# puts "Введите параметры перевозимого груза - вес(кг), длина(см), ширина(см), высота(см)"
+# puts Parameters.new(weight:gets.chomp, length:gets.chomp, width:gets.chomp, height:gets.chomp).show
+
 
 	# def initialize (weight, length, width, height)
 	# 	self.Weight= weight
@@ -24,14 +53,3 @@ class Parameters
 	# 	#{self.Weight}
 	# 	#{self.Height}"
 	# end
-
-	def show
-		"Вес #{self.Weight} кг
-Длина #{self.Length} см
-Ширина #{self.Width} см
-Высота #{self.Height} см"
-	end
-
-end
-puts "Введите параметры перевозимого груза - вес(кг), длина(см), ширина(см), высота(см)"
-puts Parameters.new(weight:gets.chomp, length:gets.chomp, width:gets.chomp, height:gets.chomp).show
